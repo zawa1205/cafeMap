3768f6d25b6a702bc4a21b1fc7a239929958d574
zawa1205 <aizu.serizawa@gmail.com>
2020-02-02 00:58:36 +0900

    firebase setting

--- cafe-map/src/main.js
+++ cafe-map/src/main.js
@@ -3,8 +3,23 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC_9JAOx8-eRswaTLkQtv1qkpoRA7xOllI",
+  authDomain: "cafemap-zawa.firebaseapp.com",
+  databaseURL: "https://cafemap-zawa.firebaseio.com",
+  projectId: "cafemap-zawa",
+  storageBucket: "cafemap-zawa.appspot.com",
+  messagingSenderId: "683160247793",
+  appId: "1:683160247793:web:3ba54c0be0379d326cb850",
+  measurementId: "G-SZZTNRFW7L"
+}
+
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
 
 new Vue({
   router,
